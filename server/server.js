9002cb9c280170d325e0abaf46116a1a74febafa
NickMannhardt <30861443+NickMannhardt@users.noreply.github.com>
2020-01-12 11:21:36 -0500

    Connected to MongoDB
    used account:
    username: niklasm@mit.edu
    password: JonesGustafsonMannhardt
    
    login and logout works without error

--- server/server.js
+++ server/server.js
@@ -33,9 +33,9 @@ const socket = require("./server-socket");
 
 // Server configuration below
 // TODO change connection URL after setting up your team database
-const mongoConnectionURL = "FILL ME IN";
+const mongoConnectionURL = "mongodb+srv://niklasmannhardt:16flammingo309@cluster0-y8uhl.mongodb.net/test?retryWrites=true&w=majority";
 // TODO change database name to the name you chose
-const databaseName = "FILL ME IN";
+const databaseName = "flip";
 
 // connect to mongodb
 mongoose
