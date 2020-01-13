5bab3c1ca811072e850f3d64bc1793e46999bb9b
NickMannhardt <30861443+NickMannhardt@users.noreply.github.com>
2020-01-12 20:13:01 -0500

    very basic drawing
    can draw on canvas with mouse, no clicking yet, so hovering over the canvas automatically paints. baby steps

--- client/src/components/pages/Studio.js
+++ client/src/components/pages/Studio.js
@@ -3,27 +3,79 @@ import React, { Component } from "react";
 import "../../utilities.css";
 import "./Studio.css";
 
-class Studio extends Component {
-  constructor(props) {
-    super(props);
-    // Initialize Default State
-    this.state = {};
 
-  }
-
-  componentDidMount() {
-    // remember -- api calls go here!
-
-  }
-
-  render() {
-    return (
-      <>
-        <h1>STUDIO</h1>
-        <h2> This page is for creating flipbooks.</h2>
-      </>
-    );
-  }
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
 }
 
-export default Studio;
+class Studio extends React.Component {
+    constructor(props) {
+        super(props);
+        this.canvasRef = React.createRef();
+    }
+  
+    componentDidMount() {
+        const canvas = this.canvasRef.current;
+        const ctx = canvas.getContext('2d');
+        ctx.fillStyle = "White";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        
+
+    }
+
+    componentDidUpdate() {
+        const canvas = this.canvasRef.current;
+        const ctx = canvas.getContext('2d');
+        canvas.addEventListener('mousemove', function(event) {
+            const mouse = getMousePos(canvas, event);
+            console.log(mouse.x + ", " + mouse.y);
+            ctx.fillStyle = "Black";
+            ctx.fillRect(mouse.x, mouse.y, 5, 5);
+            // ctx.fill();
+        });
+    }
+  
+    render() {
+      return (
+        <>
+            <div class="CanvasContainer">
+                <canvas width="700" height="500" ref={this.canvasRef} class="Canvas" />
+            </div>
+        </>
+      )
+      
+    }
+  }
+  
+
+class Animation extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { angle: 0 };
+      this.updateAnimationState = this.updateAnimationState.bind(this);
+    }
+  
+    componentDidMount() {
+      this.rAF = requestAnimationFrame(this.updateAnimationState);
+    }
+  
+    updateAnimationState() {
+      this.setState(prevState => ({ angle: prevState.angle + 1 }));
+      this.rAF = requestAnimationFrame(this.updateAnimationState);
+    }
+  
+    componentWillUnmount() {
+      cancelAnimationFrame(this.rAF);
+    }
+  
+    render() {
+      return <Studio angle={this.state.angle} />;
+    }
+}
+
+export default Animation;
+// ReactDOM.render(<Animation />, document.body);
