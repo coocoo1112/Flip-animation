1f382191dd4fbdaf86c180445919db0002edb50d
NickMannhardt <30861443+NickMannhardt@users.noreply.github.com>
2020-01-14 11:06:17 -0500

    start profile page

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { get } from "../../utilities";
 
 import "../../utilities.css";
 import "./Studio.css";
@@ -6,18 +7,25 @@ import "./Studio.css";
 class Profile extends Component {
   constructor(props) {
     super(props);
-    // Initialize Default State
-    this.state = {};
+    this.state = {
+      user: undefined,
+      catHappiness: 0,
+    };
   }
 
   componentDidMount() {
     // remember -- api calls go here!
+    get(`/api/user`, { userid: this.props.userId }).then((user) => this.setState({ user: user }));
   }
 
   render() {
+    if (!this.state.user) {
+      return <div> Loading! </div>;
+    }
     return (
       <>
         <h1>Profile</h1>
+        <h1>{this.state.user.name}</h1>
         <h2> This page is for displaying user information.</h2>
       </>
     );
