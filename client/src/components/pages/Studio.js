a652cce6ce3892eb9de2c3b33f0552eaa1a21c13
NickMannhardt <30861443+NickMannhardt@users.noreply.github.com>
2020-01-17 13:53:28 -0500

    Put canvas in seperate component
    still need to pass parameters to make frame changes work

--- client/src/components/pages/Studio.js
+++ client/src/components/pages/Studio.js
@@ -2,17 +2,9 @@ import React, { Component } from "react";
 
 import "../../utilities.css";
 import "./Studio.css";
-import ToolNavBar from "../modules/ToolNavBar.js"
-import ThumbnailBar from "../modules/ThumbnailBar.js"
-
-
-function getMousePos(canvas, evt) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-      x: evt.clientX - rect.left,
-      y: evt.clientY - rect.top
-    };
-}
+import FlipCanvas from "../modules/FlipCanvas.js";
+import ToolNavBar from "../modules/ToolNavBar.js";
+import ThumbnailBar from "../modules/ThumbnailBar.js";
 
 function changeColor(ctx, e) {
   ctx.strokeStyle = e.target.id
@@ -91,16 +83,10 @@ class Studio extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          mouse_coord: {
-           previous_x: null,
-           previous_y: null,
-          },
           mouseDown: false,
           color: "000000",
           canvas: null,
           currentFrame: 0,
-          frames: [null],
-          frameURLs: [null],
         };
         this.canvasRef = React.createRef();
         this.canvas = null;
@@ -108,62 +94,23 @@ class Studio extends React.Component {
     }
   
     componentDidMount() {
-        this.canvas = this.canvasRef.current;
-        const ctx = this.canvas.getContext('2d');
-        this.ctx = ctx;
-        ctx.fillStyle = "White";
-        ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-        ctx.beginPath();
-        this.canvas.addEventListener('pointerleave', (event) => {
-          this.state.mouse_coord.previous_x = null;
-          this.state.mouse_coord.previous_y = null;
-        })
-        this.canvas.addEventListener('pointerdown', (event) => {
-          this.state.mouseDown = true;
-          ctx.beginPath();
-          this.state.mouse_coord.previous_x = null;
-          this.state.mouse_coord.previous_y = null;
-        })
-        this.canvas.addEventListener('pointerup', (event) => {
-          this.state.mouseDown = false;
-        })
-        this.canvas.addEventListener('pointermove', (event) => {
-            // console.log(event.buttons)
-            const mouse = getMousePos(this.canvas, event);
-            
-            if (this.state.mouseDown){
-
-              if ((this.state.mouse_coord.previous_x != null) && 
-              ((this.state.mouse_coord.previous_x != mouse.x) || (this.state.mouse_coord.previous_y != mouse.y))) {
-                ctx.moveTo(this.state.mouse_coord.previous_x, this.state.mouse_coord.previous_y);
-                ctx.lineTo(mouse.x, mouse.y);
-                ctx.stroke(); 
-              }
-              if ((this.state.mouse_coord.previous_x != mouse.x) || (this.state.mouse_coord.previous_y != mouse.y)){
-                this.setState({
-                  mouse_coord: {
-                    previous_x: mouse.x,
-                    previous_y: mouse.y,
-                  },
-                  mouseDown: this.state.mouseDown,
-                });
-              }
-            }
-        });
     }
   
     render() {
       return (
         <>
             {/* <div className="projectName">Project Title</div> */}
-            <div class="CanvasContainer">
+            {/* <div class="CanvasContainer">
                 
               <div className="Shadow3"></div>
               <div className="Shadow2"></div>
               <div className="Shadow1"></div>
               <canvas width="700" height="500" ref={this.canvasRef} class="Canvas" />
               
-            </div>
+            </div> */}
+            <FlipCanvas
+              className="flipCanvas"
+            />
             <div>
               <button onClick={() => previousFrame(this.state, this.canvas, this.ctx)}>Previous</button>
               <button onClick={() => nextFrame(this.state, this.canvas, this.ctx)}>Next</button>
@@ -213,4 +160,4 @@ class Animation extends React.Component {
     }
 }
 
-export default Animation;
+export default Studio;
