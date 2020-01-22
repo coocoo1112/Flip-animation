9b31544749c212dda456fe49ddd68ae462678995
NickMannhardt <30861443+NickMannhardt@users.noreply.github.com>
2020-01-22 16:25:49 -0500

    transparent layer of previous frame over canvas

--- client/src/components/modules/FlipCanvas.js
+++ client/src/components/modules/FlipCanvas.js
@@ -25,6 +25,9 @@ class FlipCanvas extends Component {
         this.canvasRef = React.createRef();
         this.canvas = null;
         this.ctx = null;
+        this.canvasRef2 = React.createRef();
+        this.canvas2 = null;
+        this.ctx2 = null;
     }
 
     componentDidMount() {
@@ -34,21 +37,27 @@ class FlipCanvas extends Component {
         ctx.fillStyle = "White";
         ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
         ctx.beginPath();
+
+        this.canvas2 = this.canvasRef2.current;
+        this.ctx2 = this.canvas2.getContext('2d');
+        this.ctx2.globalAlpha = 0.2;
+        // this.ctx2.fillStyle = "red";
+        // this.ctx2.fillRect(0, 0, this.canvas2.width, this.canvas2.height);
         
-        this.canvas.addEventListener('pointerleave', (event) => {
+        this.canvas2.addEventListener('pointerleave', (event) => {
           this.state.mouse_coord.previous_x = null;
           this.state.mouse_coord.previous_y = null;
         })
-        this.canvas.addEventListener('pointerdown', (event) => {
+        this.canvas2.addEventListener('pointerdown', (event) => {
           this.state.mouseDown = true;
           ctx.beginPath();
           this.state.mouse_coord.previous_x = null;
           this.state.mouse_coord.previous_y = null;
         })
-        this.canvas.addEventListener('pointerup', (event) => {
+        this.canvas2.addEventListener('pointerup', (event) => {
           this.state.mouseDown = false;
         })
-        this.canvas.addEventListener('pointermove', (event) => {
+        this.canvas2.addEventListener('pointermove', (event) => {
             const mouse = getMousePos(this.canvas, event);
             ctx.strokeStyle = this.props.color;
             if (this.state.mouseDown){
@@ -72,7 +81,7 @@ class FlipCanvas extends Component {
         });
     }
 
-    loadFrame = (frameNumber) => {
+    loadFrame = (frameNumber, shadow=false) => {
         const image = new Image();
         const ctx = this.canvas.getContext("2d");
 
@@ -82,6 +91,20 @@ class FlipCanvas extends Component {
                 ctx.drawImage(image, 0, 0);
             }
         }
+
+        if (shadow && this.props.frames[frameNumber - 1]) {
+            const image2 = new Image();
+            const ctx2 = this.canvas2.getContext("2d");
+            
+            image2.src = this.props.frames[frameNumber - 1];
+            ctx2.clearRect(0, 0, this.canvas2.width, this.canvas2.height);
+            image2.onload = function () {
+                ctx2.drawImage(image2, 0, 0);
+            }
+        } else {
+            this.ctx2.clearRect(0, 0, this.canvas2.width, this.canvas2.height);
+        }
+        
     }
 
     blankCanvas() {
@@ -95,12 +118,13 @@ class FlipCanvas extends Component {
         if (this.props.newFrame) {
             this.props.saveFrame(this.canvas, this.props.currentFrame-1);
             this.blankCanvas();
+            this.loadFrame(this.props.currentFrame, true);
             this.props.setNewFrameFalse();
         }
 
         if (this.props.switchFrame) {
             this.props.saveFrame(this.canvas, this.props.prevFrame);
-            this.loadFrame(this.props.currentFrame);
+            this.loadFrame(this.props.currentFrame, true);
             this.props.setSwitchFrameFalse();
         }
 
@@ -145,8 +169,10 @@ class FlipCanvas extends Component {
               <div className="Shadow3"></div>
               <div className="Shadow2"></div>
               <div className="Shadow1"></div>
-              <canvas width="700" height="500" ref={this.canvasRef} class="Canvas" />
               
+              <canvas width="700" height="500" ref={this.canvasRef} class="Canvas" />
+
+              <canvas width="700" height="500" ref={this.canvasRef2} class="Canvas"/>
             </div>
         )
     }
